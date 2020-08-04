e1e6e8f148b15c542998a89473af8189d569d1df
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-08-04 21:48:33 +0500

    Seed Meal Controller with dummy data

--- GetFitAPI/Controllers/MealController.cs
+++ GetFitAPI/Controllers/MealController.cs
@@ -1,5 +1,6 @@
 using GetFitAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace GetFitAPI.Controllers
 {
@@ -7,11 +8,20 @@ namespace GetFitAPI.Controllers
     [Route("[controller]")]
     public class MealController : ControllerBase
     {
-        private static Meal meal = new Meal();
+        private static List<Meal> meals = new List<Meal> {
+            new Meal(){ MealID = 1 , Calories = 100, MealType = new MealType(){ MealTypeID = 1 , Name = "Dinner"}, Name = "Kebabs"},
+            new Meal(){ MealID = 2 , Calories = 200, MealType = new MealType(){ MealTypeID = 1 , Name = "Dinner"}, Name = "Fries"}
+        };
 
+        [Route("GetAll")]
         public IActionResult Get()
         {
-            return Ok(meal);
+            return Ok(meals);
+        }
+
+        public IActionResult GetSingle()
+        {
+            return Ok(meals[1]);
         }
     }
 }
