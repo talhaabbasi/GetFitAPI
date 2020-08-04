83ae0b36d71da909afaf1ddf42d367031fc4ebac
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-08-04 21:18:28 +0500

    Meal Model added

---
+++ GetFitAPI/Models/Meal.cs
@@ -0,0 +1,9 @@
+namespace GetFitAPI.Models
+{
+    public class Meal
+    {
+        public int MealID { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+    }
+}
